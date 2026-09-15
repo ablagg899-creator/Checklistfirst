84b52623236108371adc69718ab3b5ce4a917495
ablagg899-creator <ablagg899@gmail.com>
2026-09-15 14:47:02 -0400

    Configure Asterfall Android app

---
+++ asterfall-rpg-v2/app/build.gradle.kts
@@ -0,0 +1,4 @@
+plugins { id("com.android.application") }
+android { namespace="com.asterfall.rpg"; compileSdk=35
+ defaultConfig { applicationId="com.asterfall.rpg"; minSdk=24; targetSdk=35; versionCode=3; versionName="1.0" }
+}
